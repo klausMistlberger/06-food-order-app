d8a95eefeceaa7e5f48c9d1c80d43ae7ae21c2aa
klausMistlberger <klaus.mistlberger@pm.me>
2022-07-04 10:19:10 +0200

    foa

---
+++ src/components/Meals/AvailableMeals.js
@@ -0,0 +1,27 @@
+import DUMMY_MEALS from './dummy-meals.js';
+
+import Card from '../UI/Card.js';
+import MealItem from './MealItem/MealItem.js';
+import classes from './AvailableMeals.module.css';
+
+const AvailableMeals = () => {
+  const mealsList = DUMMY_MEALS.map((meal) => (
+    <MealItem
+      key={meal.id}
+      id={meal.id}
+      name={meal.name}
+      description={meal.description}
+      price={meal.price}
+    />
+  ));
+
+  return (
+    <section className={classes.meals}>
+      <Card>
+        <ul>{mealsList}</ul>
+      </Card>
+    </section>
+  );
+};
+
+export default AvailableMeals;
---
+++ src/components/Meals/dummy-meals.js
@@ -0,0 +1,28 @@
+const DUMMY_MEALS = [
+  {
+    id: 'm1',
+    name: 'Baba Ganoush',
+    description: 'A crafty eggplant dip',
+    price: 6.9,
+  },
+  {
+    id: 'm2',
+    name: 'Eiernockerl with green salad',
+    description: 'Eggy noodles',
+    price: 8.9,
+  },
+  {
+    id: 'm3',
+    name: 'Lentil Curry',
+    description: 'A curry made of lentils',
+    price: 7.9,
+  },
+  {
+    id: 'm4',
+    name: 'Okra Curry',
+    description: 'A dish made with okras',
+    price: 10.9,
+  },
+];
+
+export default DUMMY_MEALS;
