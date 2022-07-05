320706f19c0f3856c3f6139689a8e221f8af38b8
klausMistlberger <klaus.mistlberger@pm.me>
2022-07-05 17:39:35 +0200

    fetch

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,11 +1,33 @@
-import DUMMY_MEALS from './dummy-meals.js';
-
 import Card from '../UI/Card.js';
 import MealItem from './MealItem/MealItem.js';
 import classes from './AvailableMeals.module.css';
+import { useEffect, useState } from 'react';
+
+const DB_URL =
+  'https://react-http-d3de3-default-rtdb.europe-west1.firebasedatabase.app/meals.json';
 
 const AvailableMeals = () => {
-  const mealsList = DUMMY_MEALS.map((meal) => (
+  const [fetchedMeals, setFetchedMeals] = useState([]);
+
+  useEffect(() => {
+    const fetchMeals = async () => {
+      const fetchedData = await fetch(DB_URL);
+      const data = await fetchedData.json();
+      // console.log(data);
+
+      const meals = [];
+
+      for (const key in data) {
+        meals.push({ ...data[key], id: key });
+      }
+
+      // console.log(meals);
+      setFetchedMeals(meals);
+    };
+    fetchMeals();
+  }, []);
+
+  const mealsList = fetchedMeals.map((meal) => (
     <MealItem
       key={meal.id}
       id={meal.id}
